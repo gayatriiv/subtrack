585506808bd1beff2ea6f6c9ce0368d1598dc679
Gayatri Vinod <155874924+gayatriiv@users.noreply.github.com>
2025-03-30 17:12:59 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -47,7 +47,7 @@ CREATE TABLE shared_subscription_participants (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- Create budget_goals table
+
 CREATE TABLE budget_goals (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
